f8a3d03c0c92958810c8c2ea7937072ec0cd0fb2
DubsG <gichuhigeorge041@gmail.com>
2023-05-06 22:47:34 +0000

    On hold practices for C programming.

---
+++ rocks.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * puts - prints a string to standard output
+ * main - represents the function that returns somethins
+ * Return: 0 on s/\s\+$//eiccess
+ */
+int main ()
+{
+	puts("C Rocks!");
+	return (0);
+}
